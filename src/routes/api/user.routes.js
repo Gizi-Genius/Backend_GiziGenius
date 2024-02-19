7a8c25336c2c7249e16669d1d24eb054801716c7
Genoschan1 <muhammadirfanfadhlurrahman@gmail.com>
2024-02-20 00:12:19 +0800

    Update Endpoint [BE]

--- src/routes/api/user.routes.js
+++ src/routes/api/user.routes.js
@@ -2,26 +2,25 @@ const {
   Createmakanan,
   GetallMakanan,
   updateUserProfile,
-  AddUserProfile
+  AddUserProfile,
 } = require("../../controller/backend/user.controller");
 const {
   findAllUser,
-  searchMakanan
-} = require("../../controller/backend/filter.controller")
+  searchMakanan,
+} = require("../../controller/backend/filter.controller");
 const authjwt = require("../../middlewares/auth.jwt");
 
 module.exports = (express, app, default_router) => {
   const router = express.Router();
 
   // Users
-  router.get("/users", findAllUser)
+  router.get("/users", findAllUser);
   router.put("/user-profile", [authjwt], updateUserProfile);
   router.post("/user-profile", [authjwt], AddUserProfile);
 
   //Makanan
-  router.post("/makanan", [authjwt], Createmakanan);
+  router.post("/makanan/add", [authjwt], Createmakanan);
   router.get("/makanan", [authjwt], GetallMakanan);
-
 
   app.use(default_router, router);
 };
